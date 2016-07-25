eea6bdd1da7ed8ccb83be6d9de9d60a2911b25a9
MOONYAN <1787hz@gmail.com>
2016-07-25 16:39:54 +0800

    ListItem

---
+++ js/ListItem.js
@@ -0,0 +1,44 @@
+﻿var database = firebase.database();
+var $itemContainer = $('#_itemContainer');
+/*
+firebase.database().ref('item').once('value').then(function (snapshot) {
+
+    snapshot.forEach(function (childSnapshot) {
+        var data = childSnapshot.val();
+        var $item = $('<div class="col-xs-12 col-sm-4 list-group"></div>');
+        console.log(snapshot.key);
+        var $id = $(String.format('<div class="list-group-item">{0}</div>', childSnapshot.key));
+        console.log(snapshot.Name);
+        var $name = $(String.format('<div class="list-group-item">{0}</div>', data.Name));
+        console.log(snapshot.UnitPrice);
+        var $price = $(String.format('<div class="list-group-item">{0}</div>', data.UnitPrice));
+        console.log(snapshot.ImageURL);
+        var $image = $(String.format('<div><img class="img-circle" alt="imageSource" src={0} /><div>', data.ImageURL));
+
+        $itemContainer.append($item);
+        $item.append($image);
+        $item.append($id);
+        $item.append($name);
+        $item.append($price);     
+    });      
+});
+*/
+
+firebase.database().ref('item').on('child_added',function (snapshot) {
+        var data = snapshot.val();
+        var $item = $('<div class="col-xs-12 col-sm-4 list-group"></div>');
+        console.log(snapshot.key);
+        var $id = $(String.format('<div class="list-group-item">ItemId:{0}</div>', snapshot.key));
+        console.log(snapshot.Name);
+        var $name = $(String.format('<div class="list-group-item">Name:{0}</div>', data.Name));
+        console.log(snapshot.UnitPrice);
+        var $price = $(String.format('<div class="list-group-item">UnitPrice:{0}</div>', data.UnitPrice));
+        console.log(snapshot.ImageURL);
+        var $image = $(String.format('<div><img class="img-circle" alt="imageSource" src={0} /><div>', data.ImageURL));
+
+        $itemContainer.append($item);
+        $item.append($image);
+        $item.append($id);
+        $item.append($name);
+        $item.append($price);
+});
