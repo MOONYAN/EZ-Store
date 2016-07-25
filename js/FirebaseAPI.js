25824d733c204a44ef9e83b293799109a064cb05
MOONYAN <1787hz@gmail.com>
2016-07-25 13:28:58 +0800

    PostItem page

---
+++ js/FirebaseAPI.js
@@ -0,0 +1,8 @@
+﻿// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyD0OLMqD0KMT5B-ANSRHLz-SJRSFtqELOk",
+    authDomain: "ez-shopping-db.firebaseapp.com",
+    databaseURL: "https://ez-shopping-db.firebaseio.com",
+    storageBucket: "ez-shopping-db.appspot.com",
+};
+firebase.initializeApp(config);
---
+++ js/PostItem.js
@@ -0,0 +1,14 @@
+﻿var database = firebase.database();
+
+
+$('#_pushButton').click(function ()
+{
+    var newItem =
+        {
+            Name: $('#_nametext').val(),
+            ImageURL: $('#_imageUrlText').val(),
+            UnitPrice: $('#_unitPriceText').val(),
+        };
+    database.ref('item').push(newItem);
+    location.reload();
+});
