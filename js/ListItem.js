b3a046e0e19c67de838d61925ede9dbee33a6c3d
MOONYAN <1787hz@gmail.com>
2016-07-31 15:02:04 +0800

    generate QR

--- js/ListItem.js
+++ js/ListItem.js
@@ -25,20 +25,26 @@ firebase.database().ref('product').once('value').then(function (snapshot) {
 */
 
 firebase.database().ref('product').on('child_added', function (snapshot) {
-        var data = snapshot.val();
-        var $item = $('<div class="col-xs-12 col-sm-4 list-group"></div>');
-        console.log(snapshot.key);
-        var $id = $(String.format('<div class="list-group-item">{0}</div>', snapshot.key));
-        console.log(snapshot.Name);
-        var $name = $(String.format('<div class="list-group-item">{0}</div>', data.Name));
-        console.log(snapshot.UnitPrice);
-        var $price = $(String.format('<div class="list-group-item">{0}</div>', data.UnitPrice));
-        console.log(snapshot.ImageURL);
-        var $image = $(String.format('<div><img class="img-circle" alt="imageSource" src={0} /><div>', data.ImageURL));
+    var data = snapshot.val();
+    var $item = $('<div class="col-xs-12 col-sm-4 list-group"></div>');
+    console.log(snapshot.key);
+    var $id = $(String.format('<div class="list-group-item">{0}</div>', snapshot.key));
+    console.log(snapshot.Name);
+    var $name = $(String.format('<div class="list-group-item">{0}</div>', data.Name));
+    console.log(snapshot.UnitPrice);
+    var $price = $(String.format('<div class="list-group-item">{0}</div>', data.UnitPrice));
+    console.log(snapshot.ImageURL);
+    var $image = $(String.format('<div><img class="img-circle" alt="imageSource" src={0} /><div>', data.ImageURL));
+    var $qr = $('<div></div>');
+    $qr.qrcode(
+        {
+            width: '128', height: '128', text: snapshot.key
+        });
 
-        $itemContainer.append($item);
-        $item.append($image);
-        $item.append($id);
-        $item.append($name);
-        $item.append($price);
+    $itemContainer.append($item);
+    $item.append($image);
+    $item.append($qr);
+    $item.append($id);
+    $item.append($name);
+    $item.append($price);
 });
